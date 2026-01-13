e04953ca672bce1a3d5b88ec1d716f9a09bcba5b
Proxin <thugshaker627@gmail.com>
2026-01-13 21:55:51 +0100

    fix: switched to busybox

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ initd is a minimalist init system for linux, similar to runit it has 3 stages, t
 ## Development
 To test initd during development you can either use the [automated script](scripts/run-qemu.sh) or follow the following steps:
 - Create a rootfs directory with all the needed directories (eg. /bin, /sbin, /etc/initd, and so on)
-- Copy [toybox](https://landley.net/toybox/) utilities into /bin
+- Copy a static busybox build into /bin
 - Copy the initd binary from target/x86_64-unknown-linux-musl/release/initd into /sbin/init
 - Build initramfs archive of rootfs
 - Boot qemu with initrd set to the initramfs archive
--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -10,18 +10,18 @@ rm -rf rootfs
 mkdir -p rootfs/{bin,sbin,etc,proc,sys,dev,run,tmp}
 mkdir -p rootfs/etc/initd/{boot,supervise,shutdown,disabled}
 
-cp /usr/bin/toybox rootfs/bin/toybox
-chmod +x rootfs/bin/toybox
+cp /usr/bin/busybox rootfs/bin/busybox
+chmod +x rootfs/bin/busybox
 
-for cmd in $(rootfs/bin/toybox | tr ' ' '\n' | tail -n +2); do
+for cmd in $(rootfs/bin/busybox --list | tr ' ' '\n' | tail -n +2); do
     if [ -n "$cmd" ]; then
-        ln -sf rootfs/bin/toybox rootfs/bin/$cmd
+        ln -sf /bin/busybox rootfs/bin/$cmd
     fi
 done
 
 cp /usr/bin/dash rootfs/bin/dash
 chmod +x rootfs/bin/dash
-ln -sf rootfs/bin/dash rootfs/bin/sh
+ln -sf /bin/dash rootfs/bin/sh
 
 cp target/x86_64-unknown-linux-musl/release/initd rootfs/sbin/init
 chmod +x rootfs/sbin/init
